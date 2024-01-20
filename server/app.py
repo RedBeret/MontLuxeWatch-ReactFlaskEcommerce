4f334dc014ed0ffc86c80cc48867062305b98f16
Steven Espinoza <espisteven5@gmail.com>
2024-01-19 16:44:49 -0800

    setup part 2

--- server/app.py
+++ server/app.py
@@ -2,22 +2,24 @@
 
 # Standard library imports
 
+# Local imports
+from config import api, app, db
+
 # Remote library imports
 from flask import request
+from flask_cors import cross_origin
 from flask_restful import Resource
 
-# Local imports
-from config import app, db, api
 # Add your model imports
 
 
 # Views go here!
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Project Server</h1>'
+    return "<h1>Project Server</h1>"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
